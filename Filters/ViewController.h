e4432ec0d06159cea5af25ecaa78dec9b4ebe563
Steven Hickson <me@stevenhickson.com>
2014-06-07 23:18:06 -0700

    First commit. Working baseline

---
+++ Filters/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Filters
+//
+//  Created by Steven Hickson on 6/7/14.
+//  Copyright (c) 2014 Fyusion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate>
+
+@end
